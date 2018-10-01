d7747c7581deddc1abfdbd3b7540bfae9166bd11
BrotherWolf8 <daniel.w.innes@gmail.com>
2018-10-01 14:05:50 -0400

    fix import and JUnit

--- tests/CarTest.java
+++ tests/CarTest.java
@@ -4,12 +4,12 @@
  * and open the template in the editor.
  */
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
--- tests/SeatTest.java
+++ tests/SeatTest.java
@@ -4,12 +4,10 @@
  * and open the template in the editor.
  */
 
-import org.junit.After;
-import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
